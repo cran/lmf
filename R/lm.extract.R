52eb8140e359350d7c618865349597501f388e1d
Thomas Kvalnes <thomas.kvalnes@bio.ntnu.no>
2012-04-26 00:00:00 +0000

    version 1.0

---
+++ R/lm.extract.R
@@ -0,0 +1,25 @@
+lm.extract <-
+function(formula, data, na.action = na.exclude)
+    {
+      #"mod" is a fitted linear model
+      mod <- lm(formula = formula, data = data, na.action = na.action)
+      #Alphas
+      ret <- list(ajt = mod$coefficients)
+      #Residuals
+      ret$res <- residuals(mod)
+      #Fitted values
+      ret$fit <- predict(mod)
+      #Degrees of freedom
+      ret$dof <- df.residual(mod)
+      #Residual standard error
+      ret$sigma.djt <- sqrt(deviance(mod)/ret$dof)
+      #Unscaled covariance matrices (the variance-covariance
+      #matrix is given by "cov.unscaled * sigma^2")
+      ret$Ajt.us <- vcov(mod)/ret$sigma.djt^2
+      #Leverage
+      ret$leverage <- lm.influence(mod, do.coef = FALSE)$hat
+      #Cooks distance
+      ret$cook <- cooks.distance(mod, sd = ret$sigma.djt, res = ret$res)
+      #Output
+      ret
+    }
